1c1c55d7c8cb8e3ac0044b33b6624f283060ce31
padrocha <p.roch@outlook.es>
2025-03-01 20:21:12 -0600

    routes

---
+++ src/app/routes/pdf/pdf.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pdf',
+  imports: [],
+  templateUrl: './pdf.component.html',
+  styleUrl: './pdf.component.scss'
+})
+export class PdfComponent {
+
+}
